cc81effd8adc4f59cea698ced65b227c711a012c
Rafal Stapinski <stapinskirafal@gmail.com>
2024-04-20 20:24:14 -0400

    smore elements and testing. VoidElement

--- pathatoes/dom.py
+++ pathatoes/dom.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Self
+from typing import Any, NoReturn, Self, TypedDict, Unpack, reveal_type
 
 
 class EventTarget:
@@ -38,9 +38,17 @@ class Element(Node):
     def __init__(
         self,
         /,
+        classes: list[str] | None = None,
         **kwargs,
     ):
-        self._attributes = kwargs
+        self._attributes = {}
+        self._children = ()
+
+        if classes:
+            self._attributes["class"] = " ".join(classes)
+
+        for key in kwargs:
+            self._attributes[key.replace("_", "-")] = kwargs[key]
 
     def __getitem__(self, children: tuple[str | Element, ...] | str | Element) -> Self:
         match children:
@@ -56,13 +64,24 @@ class Element(Node):
         return self
 
     def to_html(self) -> str:
-        res = [f"<{self._tag}>"]
+        attrs = " ".join([f'{key}="{value}"' for key, value in self._attributes.items()])
+
+        res = [f"<{self._tag} {attrs}>"]
 
         for child in self._children:
             res.append(child.to_html())
 
         res.append(f"</{self._tag}>")
         return "".join(res)
+
+
+class VoidElement(Element):
+    def __getitem__(self) -> NoReturn:
+        raise Exception(f"{self._tag} cannot have children")
+
+    def to_html(self) -> str:
+        attrs = " ".join([f'{key}="{value}"' for key, value in self._attributes.items()])
+        return f"<{self._tag} {attrs} />"
 
 
 class html(Element):
@@ -89,9 +108,125 @@ class a(Element):
     _tag = "a"
 
 
-class img(Element):
+class span(Element):
+    _tag = "span"
+
+
+class title(Element):
+    _tag = "title"
+
+
+class img(VoidElement):
     _tag = "img"
 
 
-class span(Element):
-    _tag = "span"
+class br(VoidElement):
+    _tag = "br"
+
+
+class hr(VoidElement):
+    _tag = "hr"
+
+
+class input(VoidElement):
+    _tag = "input"
+
+
+class button(Element):
+    _tag = "button"
+
+
+class form(Element):
+    _tag = "form"
+
+
+class label(Element):
+    _tag = "label"
+
+
+class select(Element):
+    _tag = "select"
+
+
+class option(Element):
+    _tag = "option"
+
+
+class textarea(Element):
+    _tag = "textarea"
+
+
+class table(Element):
+    _tag = "table"
+
+
+class tr(Element):
+    _tag = "tr"
+
+
+class th(Element):
+    _tag = "th"
+
+
+class td(Element):
+    _tag = "td"
+
+
+class ul(Element):
+    _tag = "ul"
+
+
+class ol(Element):
+    _tag = "ol"
+
+
+class li(Element):
+    _tag = "li"
+
+
+class h1(Element):
+    _tag = "h1"
+
+
+class h2(Element):
+    _tag = "h2"
+
+
+class h3(Element):
+    _tag = "h3"
+
+
+class h4(Element):
+    _tag = "h4"
+
+
+class h5(Element):
+    _tag = "h5"
+
+
+class h6(Element):
+    _tag = "h6"
+
+
+class meta(VoidElement):
+    _tag = "meta"
+
+
+class link(VoidElement):
+    _tag = "link"
+
+
+class style(Element):
+    _tag = "style"
+
+
+class script(Element):
+    _tag = "script"
+
+
+class noscript(Element):
+    _tag = "noscript"
+
+
+class iframe(Element):
+    _tag = "iframe"
--- t.py
+++ t.py
@@ -1,8 +1,49 @@
+from typing import NotRequired, TypedDict, Unpack
+
+from bs4 import BeautifulSoup
+from fastapi import FastAPI
+from fastapi.applications import HTMLResponse
+from pydantic import BaseModel
+
 from pathatoes.dom import *
 
-z = div()[
-    "thinger",
-    div()[p()["paragraph",],],
+z = html()[
+    head()[title()["Hello, world!"],],
+    body()[
+        div()[
+            p()["Hello, world!"],
+            a(href="https://google.com")["Google"],
+            img(src="https://via.placeholder.com/150"),
+            span()["Hello, world!"],
+        ],
+    ],
 ]
 
-print(z.to_html())
+
+output = z.to_html()
+
+soup = BeautifulSoup(output, "html.parser")
+
+print(soup.prettify())
+
+i = img(src="https://via.placeholder.com/150")
+
+app = FastAPI()
+
+
+@app.get("/")
+async def index() -> HTMLResponse:
+    return HTMLResponse(content=output, status_code=200)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "t:app",
+        host="0.0.0.0",
+        port=8000,
+        use_colors=True,
+        log_level="info",
+        reload=True,
+    )
